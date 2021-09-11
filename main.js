e31748b7c7d05e3c861a2c4472c0e68871df17ea
BHAVYABB14 <90505667+BHAVYABB14@users.noreply.github.com>
2021-09-11 16:41:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+name_of_the_array=[];
+
+function submit(){
+    var display_array = [];
+    
+    for (var j=1; j<=4; j++){
+
+        var name_of_the_student =document.getElementById("name_of_the_student_"+j).nodeValue;
+        console.log(name_of_the_student);
+
+        name_of_the_array.push(name_of_the_student);
+
+
+    }
+    console.log(name_of_the_array);
+
+    var length_of_the_array=name_of_the_array.length_of_the_array;
+    console.log(length_of_the_array);
+
+for (var k=0;k<length_of_the_array;k++)
+}
